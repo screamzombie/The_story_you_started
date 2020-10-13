ce50b301106e3e83c6d80485b04b85ea739a972b
rainbow <1525025692@qq.com>
2020-10-13 22:04:25 +0800

    累死我了一晚上就写了一句。。。。

---
+++ 数据结构/链表栈-new.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class MyStack
+{
+public:
+    int data;
+    MyStack *next;
+    MyStack(int data_ = 0, MyStack *next_ = nullptr) : data(data_), next(next_) {}
+    ~MyStack() {}
+};
+
+using MyStack_P = MyStack *;
+MyStack *push(MyStack_P t, int item)
+{
+    MyStack *p = new MyStack(item, nullptr);
+    p->next = t;
+    return p;
+}
+
+int pop(MyStack_P &t)
+{
+    if (t != nullptr) //如果t != nullptr
+    {
+        int temp = t->data; //说明有问题
+        t = t->next;
+        return temp;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+void Transform()
+{
+    int a = 16, b = 2;
+    cout << "请输入要转化的数" << endl;
+    cin >> a;
+    cout << "请输入要转化的进制" << endl;
+    cin >> b;
+    MyStack_P head = nullptr;
+    while (a)
+    {
+        int z = a % b;
+        head = push(head, a % b);
+        a /= b;
+    }
+    int temp = 1;
+    while ((temp = pop(head)) != -1)
+        cout << temp << " ";
+    cout << endl;
+}
+
+int main()
+{
+    Transform();
+    return 0;
+}
